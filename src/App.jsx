41413f7dbbcb70a86b2bb537a993ab482ba9494b
Prashrijan <shresthaprashrijan@gmail.com>
2024-12-04 23:15:45 +1100

    expense list add

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
+import { useState } from "react";
 import "./App.css";
+import ExpenseList from "./Components/ExpenseList";
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    {
+      description: "Standing Desk",
+      category: "utlities",
+      amount: 200,
+    },
+    {
+      description: "Bed",
+      category: "utlities",
+      amount: 400,
+    },
+    {
+      description: "Eggs",
+      category: "Grocery",
+      amount: 15,
+    },
+    {
+      description: "Movies",
+      category: "Entertainment",
+      amount: 20,
+    },
+  ]);
   return (
     <>
-      <h2>Expense Tracker</h2>
+      <div className="wrapper min-h-screen w-full bg-gradient-to-r from-gray-900 via-gray-800 to-black text-white flex flex-col items-center gap-7 py-10">
+        <h2 className="max-w-lg text-2xl md:text-3xl font-semibold leading-normal text-gray-900 dark:text-white text-center">
+          Track Your Expenses Efficiently
+        </h2>
+        <ExpenseList expenses={expenses} />
+      </div>
     </>
   );
 }
---
+++ src/Components/ExpenseList.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+const ExpenseList = ({ expenses }) => {
+  return (
+    <>
+      <div className="w-96 md:w-1/2 overflow-x-auto shadow-md rounded-lg ">
+        <table className="w-full text-md text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Expense
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Category
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Amount
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {expenses.map((expense) => {
+              return (
+                <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    {expense.description}
+                  </th>
+                  <td className="px-6 py-4">{expense.category}</td>
+                  <td className="px-6 py-4">${expense.amount}</td>
+                  <td className="px-6 py-4">
+                    <a
+                      href="#"
+                      className="font-medium text-red-600 dark:text-red-500 hover:underline"
+                    >
+                      Delete
+                    </a>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+      <div className="p-4 mb-4 text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400 flex items-center gap-4">
+        <h1 className="text-xl font-bold">Total Spent : </h1>
+        <span className="text-xl text-blue-50">
+          ${expenses.reduce((total, item) => total + item.amount, 0)}
+        </span>
+      </div>
+    </>
+  );
+};
+
+export default ExpenseList;
